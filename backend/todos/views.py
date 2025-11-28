b438afa338951ebd7726305c7bdc09a6564012b2
Rosa Lopez <38490966+rosa-lpz@users.noreply.github.com>
2025-11-27 17:41:53 -0700

    models, serializers, views and urls added

--- backend/todos/views.py
+++ backend/todos/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Todo
+from .serializers import TodoSerializer
 
-# Create your views here.
+class TodoList(APIView):
+    def get(self, request):
+        todos = Todo.objects.all()
+        serializer = TodoSerializer(todos, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class TodoDetail(APIView):
+    def get(self, request, pk):
+        todo = Todo.objects.get(pk=pk)
+        serializer = TodoSerializer(todo)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        todo = Todo.objects.get(pk=pk)
+        serializer = TodoSerializer(todo, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        todo = Todo.objects.get(pk=pk)
+        todo.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
